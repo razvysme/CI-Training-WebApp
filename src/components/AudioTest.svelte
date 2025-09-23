ef51527d3f89ede6d9a7c1236cd1e4e108c5fad1
Razvan Paisa <razvysme@yahoo.com>
2025-09-23 11:14:32 +0200

    Changed back to SoundCore welcoming page

--- src/components/AudioTest.svelte
+++ src/components/AudioTest.svelte
@@ -10,7 +10,7 @@
     let audioFile;
 
     if (browser) {
-     audioFile = new Audio("https://d3spngajmc7mtz.cloudfront.net/Chimes.mp3");
+     audioFile = new Audio("https://github.com/razvysme/CI-Training-WebApp/raw/refs/heads/main/src/lib/audio/Chimes.mp3");
     }
 
     function playTestAudio() 
@@ -30,18 +30,25 @@
 
 <form class="my-11 " on:submit|preventDefault={handleSubmit}>
     <div class="flex flex-col text-lg">
-        <h3 class="font-bold mb-2 text-center text-2xl text-gray-700">Lydtest</h3>
-        
-        <h1 class="mb-4 text-gray-700 text-lg text-justify"> Du er velkommen til at lytte til træningen som du vil – på din computer, telefon eller tablet – og ved at streame eller gennem den indbyggede højtaler. Inden du går videre, har du her muligheden for at tjekke at lyden virker.</h1>
+        <h3 class="font-bold mb-3 text-left text-cr text-gray-700">Lad os sikre os at lyden virker ved at trykke på <span class="text-gray-1000">Test Lyden</span></h3>
+        <h1 class="mb-4 text-gray-500 text-xl text-center">Hørte du en lyd?</h1>
         <h1 class="mb-2 text-gray-500 text-base text-left">Hvis ikke, så følg disse trin for at få lyden til at fungere:</h1>
-        
-        <ul class="list-disc list-inside ml-4 text-sm text-gray-500">
-          <li>Tjek at dine højttalere eller høretelefoner er tilsluttet korrekt.</li>
-          <li>Hvis du bruger en Cochlearimplant, skal du sørge for, at streaming er slået til.</li>
-          <li>Sørg for, at lydstyrken er skruet op på din enhed.</li>
-          <li>Prøv at genstarte din enhed og forsøg igen.</li>
-        </ul>
-      </div>
+
+        <div class="flex items-center mb-3">
+            <img class="w-24 h-24 rounded-full mr-2" src="https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/img/SpeakerIcon.png" alt="Speaker Image">
+            <div>
+              <h3 class="mb-0.5 text-gray-800">Tænd højtaleren</h3>
+              <p class="text-gray-500 text-xs">Hold tændknappen i toppen af højtaleren nede i tre sekunder indtil du hører "tændelyden". Hvis du ikke hører noget, er højtaleren formentlig løbet tør for strøm, så tilslut den opladekablet.</p>
+            </div>
+        </div>
+        <div class="flex items-center">
+            <img class="w-24 h-24 rounded-full mr-2" src="https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/img/BtIcon.png" alt="Speaker Image">
+            <div>
+              <h3 class="mb-0.5 text-gray-800">Forbind med højtaleren</h3>
+              <p class="text-gray-500 text-xs">Gå ind i <span class="font-bold">Indstillinger</span> på din telefon, find <span class="font-bold">Bluetooth</span> og vælg <span class="font-bold">SoundCore 2</span>. Hvis navnet ikke er på listen skal du holde <span class="font-bold">Bluetooth</span> knappen på højtaleren nede indtil højtaleren begynder at blinke. Derefter skal vente indtil den dukker op på listen så du kan vælge den på telefonen.</p>
+            </div>
+        </div>
+    </div>
     <div class="flex justify-between space-x-2">
         <button
           type="button"
@@ -57,8 +64,4 @@
           Fortsæt
         </button>
       </div>
-      <div class="mt-0">
-        <Logos />
-    </div>
-
 </form>
--- src/components/LoginForm.svelte
+++ src/components/LoginForm.svelte
@@ -43,35 +43,16 @@
 <form class="my-11" on:submit|preventDefault={handleSubmit}>
   <div class="flex flex-col text-lg mb-1">
     <h3 class="font-bold mb-3 text-center text-xl text-gray-700">Velkomme til siden for musiklytning træningsprogram</h3>
-    <h3 class="font-regular text-justify text-gray-700">Inden du starter træningen skal du tage en musik-quiz. Klik her for at starte med </h3>
 
-    <!-- Start Quiz Button -->
-    <button
-      type="button"
-      class="mt-3 mb-11 w-full shadow-sm rounded bg-orange-500 hover:bg-orange-600 text-lg text-white py-2 px-4"
-      on:click={startQuiz}>
-      Start Quiz
-    </button>
 
-    <!-- Conditionally render the manual button if the quiz fails to open in a new tab -->
-    {#if showManualButton}
-    <h3 class="font-regular mb-3 text-justify text-cr text-gray-700" >Der ser ud til at være et problem med quiz siden. Prøv at åbne quiz’en manuelt. </h3>
-      <button
-        type="button"
-        class="mt-3 mb-11 w-full shadow-sm rounded bg-green-600 hover:bg-green-700 text-lg text-white py-2 px-4"
-        on:click={handleManualQuiz}>
-        Åbne quiz side
-      </button>
-    {/if}
-
-    <h3 class="font-regular mb-3 text-justify text-cr text-gray-700">Når du har taget quiz’en, skal du indtaste dit telefonnummer herunder for at fortsætte til træningen</h3>
+    <h3 class="font-regular mb-3 text-justify text-cr text-gray-700">Før vi starter, skal vi vide, hvem du er</h3>
     
     <!-- Phone Number Input -->
     <input
       type="text"
       bind:value={usr}
       name="todo"
-      placeholder="Skriv dit telefonnummer her"
+      placeholder="Skriv did dyr"
       class="appearance-none shadow-sm border border-gray-200 p-2 focus:outline-none focus:border-gray-500 rounded-lg" />
 
     <!-- Continue Button -->
